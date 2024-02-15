b75059966f1524a0e766768c8e1cf295d7f7ee53
Anton-CSS <9251580262@mail.ru>
2024-02-15 14:24:23 +0300

    test-2

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-/**
-  * Функция возвращает фрагмент алфавита
- * @param {number} from - параметр отвечающий за начало поиска
- * @param {number} to - параметр отвечающий за конец поиска
- * @param {Array} exclude - массив исключений - буквы, содержащиеся в нем, не будут включены в результирующую строку
- * @returns {String} - возвращается строка состоящая из элементов полученного массива разделенного пробелом
- **/
-const alphabetSlice = (from=0, to = 25, exclude=['c', 'd']) => {
-    const content = []
-    for(let i = from; i <= to; i++){
-        const  char = String.fromCharCode(97 + i)
-        if(!exclude.includes(char)) {
-            content.push(char)
-        }
-    }
-    return content.join(' ')
+const someArray1 = [1,2,3,4,5,6,7,8,9,10];
+const result1 = {};
+
+const result2 = someArray1.reduce((acc, item, idx) =>{
+    const index = String.fromCharCode(97 + idx);
+    acc[index] = item;
+    return acc;
+}, {});
+
+
+
+for (let i = 0; i < someArray1.length; i++) {
+    const index = String.fromCharCode(97 + i)
+    result1[index] = someArray1[i]
 }
 
-console.log(alphabetSlice())
+console.log(result1)
+console.log(result2)
+
+
+const someObject1 = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: NaN, l: 0, m: 0.25, n: null, o: {}, p: [], r: Infinity, s: undefined}
+
+const numbers = Object.values(someObject1).filter(item => typeof item === 'number' && isFinite(item));
+const naturalNumbers = Object.values(someObject1).filter(item => typeof item === 'number' && Number.isInteger(item) && item > 0);
+const objects = Object.values(someObject1).filter(item => item instanceof Object);
+
+console.log(numbers)
+console.log(naturalNumbers)
+console.log(objects)
